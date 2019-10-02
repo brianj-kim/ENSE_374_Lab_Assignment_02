39f28722dd7ced740f706d9c5724af28ff9dd9f2
Brian Kim <jiwoone@gmail.com>
2019-10-02 01:18:52 -0600

    my first commit

---
+++ linkedList.java
@@ -0,0 +1,19 @@
+/*
+ENSE 374 Lab assignment #2
+Jiwoun Kim 200329205
+
+
+: To get familier with git and github
+: Lined List in java
+
+*/
+
+import java.util.*;
+
+public class linkedList {
+    
+
+}
+    
+
+
